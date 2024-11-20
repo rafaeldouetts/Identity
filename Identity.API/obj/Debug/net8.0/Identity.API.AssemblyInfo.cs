95de8d0c1c2a7adff27e8f5c84a320b283436e27
rafaeldouetts <r_santos07@outlook.com>
2024-11-20 01:43:22 -0300

    att git ignore

--- Identity.API/obj/Debug/net8.0/Identity.API.AssemblyInfo.cs
+++ Identity.API/obj/Debug/net8.0/Identity.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Identity.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0d20a4897f4a40c91ad57a74ef3cac2acc747e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Identity.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Identity.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
