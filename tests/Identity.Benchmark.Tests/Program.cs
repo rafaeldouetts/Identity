0debeede3fa086829b91ded8c0220d59049817f6
rafaeldouetts <r_santos07@outlook.com>
2025-01-02 04:29:01 -0300

    remover conifg trivy, config feita diretamente na pipeline

--- tests/Identity.Benchmark.Tests/Program.cs
+++ tests/Identity.Benchmark.Tests/Program.cs
@@ -1,4 +1,8 @@
-﻿using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Validators;
 using Identity.Benchmark.Tests.Benchmarks;
 
 namespace Identity.Benchmark.Tests
@@ -7,6 +11,13 @@ namespace Identity.Benchmark.Tests
     {
         static void Main(string[] args)
         {
+            var config = ManualConfig.Create(DefaultConfig.Instance)
+                    .WithOptions(ConfigOptions.DisableOptimizationsValidator)
+                    .WithSummaryStyle(SummaryStyle.Default.WithCultureInfo(System.Globalization.CultureInfo.InvariantCulture))
+                    .AddExporter(HtmlExporter.Default) // Adicionando o exporter HTML
+                    .AddValidator(JitOptimizationsValidator.DontFailOnError); // Garantir que não falte configurações do JIT
+
+
             var summary = BenchmarkRunner.Run<RegistroBenchmark>();
         }
     }
